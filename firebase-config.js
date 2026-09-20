c07b85ad8b014bae12a2d7cab17cb5477abe552d
arhammullasam-cellular <arhammulla.sam@gmail.com>
2026-09-20 17:10:02 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,3 @@
-// Replace these values with your Firebase Web App configuration.
-// Firebase Web API configuration is intended for client apps, but protect your project with
-// Authentication + Firestore Security Rules and restrict any Google API key where applicable.
 export const firebaseConfig = {
   apiKey: "AIzaSyCP3p9--ECiqpNKaRpn4Y1jQ8MExSBQ1B4",
   authDomain: "wks1-ed332.firebaseapp.com",
